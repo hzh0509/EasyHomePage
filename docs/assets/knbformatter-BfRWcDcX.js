790190c44fafeed9274138036a964e3f83fbb08c
hzh0509 <hzh0509@gmail.com>
2024-06-10 10:16:26 -0400

    Force update build output

--- docs/assets/knbformatter-BfRWcDcX.js
+++
@@ -1,3 +0,0 @@
-const e={date:"2024-02-01",title:"BookTalk Prototype",url:"https://zihan-huangs-team.adalo.com/e-learning-app-app",tags:["No-Code","Adalo"]},o=`<p>Participated in the No-Code Hackathon with His friends Hongjian Yu &amp; Han Hu to design a learning software for Neurodiverse people called BookTalk, responsible for the whole UI.</p>
-`,a=`Participated in the No-Code Hackathon with His friends Hongjian Yu & Han Hu to design a learning software for Neurodiverse people called BookTalk, responsible for the whole UI.\r
-`;export{e as attributes,o as html,a as markdown};
