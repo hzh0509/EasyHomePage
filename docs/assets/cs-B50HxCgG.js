b1e7d145fbe20909829c92f83cfad954cd468471
hzh0509 <hzh0509@gmail.com>
2024-06-10 10:28:45 -0400

    Force update build output

---
+++ docs/assets/cs-B50HxCgG.js
@@ -0,0 +1,3 @@
+const e={date:"2020-06-18",title:"The Celestial Swordman",url:"https://www.zongheng.com/detail/1047898",tags:["Novel","Sci-fi"]},n=`<p>His personal novel, published on <a href="http://zongheng.com">zongheng.com</a> and was signed by the site. But he never read it a second time after finishing it.</p>
+`,t=`His personal novel, published on zongheng.com and was signed by the site. But he never read it a second time after finishing it.\r
+`;export{e as attributes,n as html,t as markdown};
